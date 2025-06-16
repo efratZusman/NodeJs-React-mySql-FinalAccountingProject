1562f33a2ed69e5824d47a231bb1d0e1f54268c0
efratZusman <avoda010120@gmail.com>
2025-06-16 21:45:10 +0300

    .

--- Server/routes/UserRoute.js
+++ Server/routes/UserRoute.js
@@ -11,20 +11,4 @@ router.post('/logout', userController.logoutUser);
 router.get('/me', getUserFromSession,userController.getCurrentUser);  
 router.patch('/subscribe-updates', getUserFromSession, userController.updateWantsUpdates);
 
-
-// Get user by username
-// router.get('/:username', userController.getUserByUsername);
-
-// Update user by username
-// router.put('/:username', userController.updateUserByUsername);
-
-// Delete user by username
-// router.delete('/:username', userController.deleteUserByUsername);
-
-// Get all users
-// router.get('/', userController.getAllUsers);
-
-// Create user
-// router.patch('/:username', userController.partialUpdateUserByUsername);
-
 module.exports = router;
